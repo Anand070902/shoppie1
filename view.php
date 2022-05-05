e6c10014d28bb6b1661450d331804ef675cecb46
Anand070902 <7anandkumarjha@gmail.com>
2022-05-05 15:52:58 +0530

    bg resolve

--- view.php
+++ view.php
@@ -20,7 +20,7 @@ $row = $callingrecord[0];
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 </head>
-<body class="bg-secondary">
+<body class="bg-light">
     <?php include "include/nav.php";?>
     <div class="container mt-4">
         <div class="row">
